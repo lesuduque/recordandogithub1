a6adc046b4423aa153f4fac10bad21698c9c1a07
TiagoCode <sospina0625@outlook.com>
2023-08-15 08:37:01 -0500

    feat: se reciben datos por teclado y se castea

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,4 @@
-#comentario de linea
+""" #comentario de linea
 nombreUsuario="Lenny Susana Duque Arango"
 edadUsuario=28
 estaturaUsuario=1.60
@@ -9,3 +9,14 @@ print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
 comidasFavoritas=["frijoles", "sushi", "mazamorra", "carne molida"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+
+#Entradas por teclado
+lugarTrabajoUsuatio=input("Digita el lugar de trabajo: ")
+print(f"Usted trabaja en: {lugarTrabajoUsuatio}") """
+
+#PEDIMOS DOS NUMEROS
+numeroUno=int(input("Digita un numero: "))
+numeroDos=int(input("Digita otro numero: "))
+sumatoria=numeroUno+numeroDos
+print(f"La suma de los números es: {numeroUno}+{numeroDos}={sumatoria}")
