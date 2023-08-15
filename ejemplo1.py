9c17ab5928538fc89b90807dc6829b20cd0bbaec
TiagoCode <sospina0625@outlook.com>
2023-08-15 08:06:15 -0500

    feat: se crean variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,11 @@
+#comentario de linea
+nombreUsuario="Lenny Susana Duque Arango"
+edadUsuario=28
+estaturaUsuario=1.60
+esHinchaDelVerde=True
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas=["frijoles", "sushi", "mazamorra", "carne molida"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
